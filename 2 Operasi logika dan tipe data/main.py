0a495d10bdbcc501571ddca68302bb58df645cd4
Rakhid16 <roploverz@gmail.com>
2020-11-16 08:02:26 +0700

    Materi pertemuan pertama

---
+++ 2 Operasi logika dan tipe data/main.py
@@ -0,0 +1,27 @@
+print("Jojo" == "jojo")
+print("Jojo" > "jojo")
+print(4 <= 5)
+print(4.0 != 4)
+
+'''
+> lebih dari
+< kurang dari
+== sama dengan
+!= tidak sama dengan
+>= lebih dari sama dengan
+<= kurang dari sama dengan
+'''
+
+betul = True
+salah = False
+
+print(type(betul))
+print(type(salah))
+print(type("Ikal"))
+print(type(16))
+print(type(3.9))
+
+print(3 * "Angka")
+
+print(len("Angka"))
+print("A" in "Angka")
