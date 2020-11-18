fc2e605d052f4a30d4f43cf71d829aef9ae563c2
Rakhid16 <roploverz@gmail.com>
2020-11-18 22:02:51 +0700

    Materi 19 November 2020

---
+++ 9 Modul dan Package/Bangun_Datar.py
@@ -0,0 +1,17 @@
+def luas_segitiga(alas, tinggi):
+  return alas * tinggi / 2
+
+def keliling_segitia(*sisi):
+  return sum(sisi)
+
+def luas_persegi(sisi):
+  return sisi ** 2
+
+def keliling_persegi(sisi):
+  return sisi * 4
+
+def luas_lingkaran(diameter):
+  return 22/7 * ((diameter / 2) ** 2)
+
+def keliling_lingkaran(diameter):
+  return 22/7 * diameter
---
+++ 9 Modul dan Package/Beli/Mobil.py
@@ -0,0 +1,5 @@
+def sedan():
+  print("Sedan telah terbeli")
+
+def jeep():
+  print("Jeep telah terbeli")
